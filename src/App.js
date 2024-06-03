010e6f27a511e9d05422f94171509da200dc0701
Nakul Attrey <nakulattrey@gmail.com>
2024-06-04 01:40:51 +0530

    Built register and login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
+import Browse from './components/Browse';
+import Register from './components/Register';
 
 function App() {
   return (
-    <div className="text-4xl font-bold text-red-700">
-      Hello Lord Nakul    
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/browse" element={<Browse />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import Header from './Header'
+
+const Login = () => {
+  return (
+    <div>
+      <Header />
+      {/* <div className="absolute">
+        <img
+          //   src="https://cdn.mos.cms.futurecdn.net/rDJegQJaCyGaYysj2g5XWY.jpg"
+          src="https://user-images.githubusercontent.com/33485020/108069438-5ee79d80-7089-11eb-8264-08fdda7e0d11.jpg"
+          alt="bg-image"
+        ></img>
+      </div> */}
+
+      <div className="absolute h-screen w-screen">
+        <div className="absolute inset-0">
+          <img
+            src="https://user-images.githubusercontent.com/33485020/108069438-5ee79d80-7089-11eb-8264-08fdda7e0d11.jpg"
+            alt="bg-image"
+            className="w-full h-full"
+          />
+          <div className="absolute inset-0 bg-black opacity-30"></div>
+        </div>
+      </div>
+
+      <form className="absolute p-12 m-12 d-flex bg-black w-4/12 my-40 mx-auto right-0 left-0 text-white bg-opacity-70">
+        <h1 className="mb-6 text-3xl font-bold">Sign In</h1>
+        <input
+          type="email"
+          placeholder="Email Address"
+          className="bg-black rounded-md border border-slate-600 p-3 m-2 w-full"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="bg-black rounded-md border border-slate-600 p-3 m-2 w-full"
+        />
+        <button className="p-2 m-2 mt-6 w-full font-semibold rounded-md bg-red-600">
+          Sign in
+        </button>
+        <p className='text-gray-400 mt-3 ml-2'>New to Netflix? <a href="/register" className='text-white'>Sign up now.</a></p>
+      </form>
+    </div>
+  );
+}
+
+export default Login
