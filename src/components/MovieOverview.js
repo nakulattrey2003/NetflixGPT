fda8d64f52a5daa87a3806ad5cfed65fe850500a
Nakul Attrey <nakulattrey@gmail.com>
2024-06-08 02:18:57 +0530

    some css changed

--- src/components/MovieOverview.js
+++ src/components/MovieOverview.js
@@ -15,7 +15,7 @@ const MovieOverview = () => {
   console.log("cast", cast);
 
   return (
-    <div className="relative w-full h-screen z-0  flex justify-center items-center overflow-hidden">
+    <div className="mt-36 w-full h-screen z-0 flex overflow-hidden text-white">
       {/* Background Image with Blur */}
       <div className="absolute inset-0 z-0">
         <img
@@ -23,38 +23,45 @@ const MovieOverview = () => {
           alt="background-image"
           className="object-cover w-full h-full fixed z-0 filter blur-sm"
         />
+        {/* Black Overlay */}
+        <div className="absolute h-full inset-0 bg-black opacity-40"></div>
       </div>
 
       {/* Movie Image and Content */}
-      <div className="z-10 w-3/4 h-full flex flex-col justify-center items-center">
+      <div className="flex flex-col">
+      <div className="z-10 w-3/4 h-full flex ">
         <img
-          className="h-12 w-12"
+          className="h-3/4 m-4 p-2 rounded-3xl"
           src={IMG_URL + movie?.poster_path}
           alt="movie-image"
         />
-        <div>{movie?.original_title}</div>
-        <div>
-          {movie?.release_date} - {movie?.vote_average} -{" "}
-          {movie?.original_language}
-        </div>
-        <div>
-          {movie.genres.map((it) => (
-            <div key={it.id}>{it.name}</div>
-          ))}
-        </div>
-        <div>{movie.overview}</div>
-        <div>
-          <div>
-            <button>Play</button>
+        <div className="flex flex-col ml-6">
+          <div className="mt-6 uppercase text-5xl font-black mb-4">
+            {movie?.original_title}
           </div>
           <div>
-            <button>Info</button>
+            {movie?.release_date} - {movie?.vote_average} -{" "}
+            {movie?.original_language}
+          </div>
+          <div>
+            {movie.genres.map((it) => (
+              <div key={it.id}>{it.name}</div>
+            ))}
+          </div>
+          <div>{movie.overview}</div>
+          <div>
+            <div>
+              <button>Play</button>
+            </div>
+            <div>
+              <button>Info</button>
+            </div>
           </div>
         </div>
       </div>
 
       {/* Cast Details */}
-      <div className="z-10 w-1/4 h-full flex flex-col justify-center items-center">
+      <div className="z-10 -mt-96 -ml-24 h-20 flex flex-row justify-center items-center space-x-4 text-white">
         {cast.cast
           .concat() // these are used to solve according to popularity  (chatgpt se kia hai)
           .sort((a, b) => b.popularity - a.popularity)
@@ -62,12 +69,17 @@ const MovieOverview = () => {
           .map((it) => (
             <div key={it.cast_id}>
               <div>
-                <img src={IMG_URL + it.profile_path} alt="character-dp" />
+                <img
+                  className="w-20 h-20 rounded-full object-cover"
+                  src={IMG_URL + it.profile_path}
+                  alt="character-dp"
+                />
               </div>
-              <div>{it.character}</div>
-              <div>{it.original_name}</div>
+              <div className="text-gray-300">{it.character}</div>
+              <div className="font-bold">{it.original_name}</div>
             </div>
           ))}
+      </div>
       </div>
     </div>
   );
