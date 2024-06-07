bbe74e331817b804ee2b110ea7b0174c7c67ec9d
Nakul Attrey <nakulattrey@gmail.com>
2024-06-07 18:06:37 +0530

    Removed the secret key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -15,6 +15,3 @@ export const SUPPORTED_LANGUAGES = [
   { identifier: "spanish", name: "Spanish" },
   { identifier: "french", name: "French" },
 ];
-
-export const OPENAI_KEY =
-  "sk-proj-q5epYhKLs0tG1jiGPPMFT3BlbkFJouB6KsuOIfLtFZmYj7Hd";
