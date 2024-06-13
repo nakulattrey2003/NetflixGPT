5bfcae0374cefa8c9f627420e600cf3ebc19854d
Nakul Attrey <nakulattrey@gmail.com>
2024-06-13 18:45:23 +0530

    added token to env file, worked after restarting app

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,10 +2,8 @@ export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    // Authorization: "Bearer " + process.env.REACT_APP_TOKEN,
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZDViYjI3NmNkZTRmNzg1OWY3Njg4Y2Q0ZDBmNmViNyIsInN1YiI6IjY2NjA1YTA4N2Q0OGY5OTdlZDI0NDMxZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oP9h0zwd7fqxlG4x9Q9P4TK1df3TkIfH-X7UPRsscHQ",
-  },
+    Authorization: "Bearer " + process.env.REACT_APP_TOKEN,
+    },
 };
 
 export const IMG_URL = "https://image.tmdb.org/t/p/w500/";
