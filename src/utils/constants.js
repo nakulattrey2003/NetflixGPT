6e50f4c834ad409906dba2cd3a4ff3b231bed596
Nakul Attrey <nakulattrey@gmail.com>
2024-06-13 18:13:34 +0530

    hide the token

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ export const API_OPTIONS = {
   headers: {
     accept: "application/json",
     Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZDViYjI3NmNkZTRmNzg1OWY3Njg4Y2Q0ZDBmNmViNyIsInN1YiI6IjY2NjA1YTA4N2Q0OGY5OTdlZDI0NDMxZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oP9h0zwd7fqxlG4x9Q9P4TK1df3TkIfH-X7UPRsscHQ",
+      `Bearer ${process.env.REACT_APP_TOKEN}`,
   },
 };
 
