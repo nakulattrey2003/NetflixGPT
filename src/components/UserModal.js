4968d9a86c1d3682a3e13b5a44d6164bff16d749
Nakul Attrey <nakulattrey@gmail.com>
2024-06-14 11:37:14 +0530

    UserModal Responsive

--- src/components/UserModal.js
+++ src/components/UserModal.js
@@ -39,35 +39,39 @@ const UserInfo = () => {
   return (
     <div className="fixed inset-0 flex items-center justify-center z-30">
       <div className="absolute inset-0 bg-black opacity-50"></div>
-      <div className="relative bg-gray-800 p-4 rounded-lg shadow-lg w-5/12 h-5/6">
+      <div className="relative bg-gray-800 p-4 rounded-lg shadow-lg w-1/2 md:w-5/12 h-2/3 md:h-5/6">
         <div className="m-2 absolute top-2 right-2">
           <button
             onClick={handleClose}
             className="text-gray-300 hover:text-white focus:outline-none"
           >
-            <IoClose size={28} />
+            <IoClose />
           </button>
         </div>
         <div className="flex flex-col items-center justify-center text-white">
-          <div className="m-4 font-bold text-3xl">{user.displayName}</div>
+          <div className="m-4 font-bold text-base md:text-3xl">
+            {user.displayName}
+          </div>
           <div className="mb-8 ">
             <img
-              className="rounded-full h-60 w-60 object-cover"
+              className="rounded-full h-24 w-24 md:h-60 md:w-60 object-cover"
               src={user.photoURL}
               alt="dp"
             />
           </div>
-          <div className="mb-7 text-xl">Email : {user.email}</div>
-          <div className="flex gap-6">
+          <div className=" md:mb-7 text-xs md:text-xl">
+            Email : {user.email}
+          </div>
+          <div className="flex gap-2 md:gap-6">
             <button
               onClick={handleLogOut}
-              className="mt-4 w-36 bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+              className="mt-4 w-15 md:w-36 bg-red-600 hover:bg-red-700 text-white text-xs md:text-base font-bold px-3 py-2 md:py-2 md:px-4 rounded"
             >
               {langArray[langKey].LogOut}
             </button>
             <button
               onClick={handleClose}
-              className="mt-4 z-30 w-36 bg-transparent text-red-500 hover:bg-red-600 hover:text-white font-bold py-2 px-4 border border-red-600 hover:border-transparent rounded"
+              className="mt-4 z-30 w-15 md:w-36 bg-transparent text-red-500 hover:bg-red-600 hover:text-white text-xs md:text-base font-bold px-3 py-2 md:py-2 md:px-4 border border-red-600 hover:border-transparent rounded"
             >
               {langArray[langKey].Close}
             </button>
--- src/pages/GptSearchPage.js
+++ src/pages/GptSearchPage.js
@@ -36,13 +36,13 @@ const GptSearchPage = () => {
   return (
     <div className="bg-gray-900 to-black min-h-screen w-full flex flex-col">
       <Header />
-      <div className="flex flex-col mt-32 md:mt-0 py-10">
-        <div className="flex items-center justify-between md:pl-4 ml-10 md:ml-28 mt-20 mb-20">
+      <div className="flex flex-col mt-20 md:mt-0 py-10">
+        <div className="flex items-center justify-between md:pl-4 ml-10 md:ml-28 md:mt-20 md:mb-20">
           <div className="border-l-4 pl-4 border-red-500 text-white text-2xl md:text-4xl font-bold">
             {langArray[langKey].SearchResults}
           </div>
         </div>
-        <div className="container mx-auto px-4">
+        <div className="container mt-10 mb:mt-0 mx-auto px-4">
           {isLoading ? (
             <SearchSkeleton /> // Show skeleton while loading
           ) : (
